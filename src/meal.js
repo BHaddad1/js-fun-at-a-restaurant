04037cd5e4ad7dfa3f3be02e258f2b986c438279
Blanche Haddad <blancherachel@gmail.com>
2022-10-12 16:27:28 -0600

    Save work again

--- src/meal.js
+++ src/meal.js
@@ -1,12 +1,19 @@
 function nameMenuItem(food) {
   return `Delicious ${food}`
+};
+
+function createMenuItem(name, price, type){
+  return menuItem = {name, price, type}
 }
 
+function addIngredients(ingredients){
+  return ingredients.push(ingredients)
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
