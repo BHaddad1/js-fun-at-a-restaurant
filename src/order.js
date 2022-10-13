b518b4bd356a89433db8fb8f05f953d2ee4e72ec
Blanche Haddad <blancherachel@gmail.com>
2022-10-13 07:22:47 -0600

    Save work in order

--- src/order.js
+++ src/order.js
@@ -1,7 +1,13 @@
 
 module.exports = {
-  // takeOrder,
+   takeOrder,
   // refundOrder,
   // listItems,
   // searchOrder
 }
+
+function takeOrder(order, deliveryOrders, takeoutOrders){
+  deliveryOrders.push(order);
+  takeoutOrders.push(order);
+  return deliveryOrders
+};
