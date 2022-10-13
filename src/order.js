0324f99527db191f2c776af5fffed05f5b22ba3e
Blanche Haddad <blancherachel@gmail.com>
2022-10-13 07:59:16 -0600

    Save work

--- src/order.js
+++ src/order.js
@@ -8,4 +8,8 @@ module.exports = {
 
 function takeOrder(order, array){
   array.push(order);
+  if (array.length > 3) {
+    return array.pop()
+    //not sure if this counts, since it's only removing the last order from the array. If there were 5 orders listed in the test, I'm not sure this would pass. 
+  }
 };
