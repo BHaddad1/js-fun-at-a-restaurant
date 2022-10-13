b51073f52dd626b86934af98abe6cf0ebcb2b467
Blanche Haddad <blancherachel@gmail.com>
2022-10-13 15:37:21 -0600

    Save work

--- src/order.js
+++ src/order.js
@@ -1,7 +1,7 @@
 
 module.exports = {
    takeOrder,
-  // refundOrder,
+   refundOrder,
   // listItems,
   // searchOrder
 }
@@ -10,6 +10,14 @@ function takeOrder(order, array){
   array.push(order);
   if (array.length > 3) {
     return array.pop()
-    //not sure if this counts, since it's only removing the last order from the array. If there were 5 orders listed in the test, I'm not sure this would pass. 
+    //not sure if this counts, since it's only removing the last order from the array. If there were 5 orders listed in the test, I'm not sure this would pass.
   }
 };
+
+function refundOrder(orderNum, array1) {
+  for (var i = 0; i < array1.length; i++) {
+    if (orderNum === array1[i].orderNumber){
+      array1.splice([i], 1);
+    }
+  }
+}
