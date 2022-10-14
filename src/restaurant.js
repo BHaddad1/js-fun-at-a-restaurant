65814467adc7a25931e5e211ce7b2f7508db8504
Blanche Haddad <blancherachel@gmail.com>
2022-10-14 13:11:18 -0600

    Save work in all three

--- src/restaurant.js
+++ src/restaurant.js
@@ -3,7 +3,7 @@
 module.exports = {
    createRestaurant,
    addMenuItem,
-  // removeMenuItem
+   removeMenuItem
 }
 
 function createRestaurant(restaurantName){
@@ -18,11 +18,26 @@ function createRestaurant(restaurantName){
   return newRestaurant
 }
 
-
 function addMenuItem(restaurantType, foodObject) {
-  if(foodObject.type === "lunch"){
-    restaurantType.menus.lunch.push(foodObject);
-  } else if(foodObject.type === "breakfast"){
-  restaurantType.menus.breakfast.push(foodObject);
-   }
-};
+  if (foodObject.type === "lunch"){
+    if (!restaurantType.menus.lunch.includes(foodObject)){
+      return restaurantType.menus.lunch.push(foodObject)}
+  return restaurantType.menus.lunch.push(foodObject)
+ } else if (foodObject.type === "breakfast"){
+    if (!restaurantType.menus.breakfast.includes(foodObject)){
+        return restaurantType.menus.breakfast.push(foodObject);
+      }
+  } else {
+    return restaurantType.menus.dinner.push(foodObject)
+  }
+  }
+
+function removeMenuItem(fromRestaurant, item, typeOfMeal){
+  for (var i = 0; i < fromRestaurant.menus[typeOfMeal].length; i++){
+    if (fromRestaurant.menus[typeOfMeal][i].name === item){
+    fromRestaurant.menus[typeOfMeal].splice([i], 1);
+    return `No one is eating our ${item} - it has been removed from the ${typeOfMeal} menu!`
+  }
+}
+return `Sorry, we don't sell ${item}, try adding a new recipe!`
+}
